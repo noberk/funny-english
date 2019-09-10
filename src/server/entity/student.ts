642bea92c5e1b1acf4d223990f7b5ed1998c123d
noberk <noberkli@gmail.com>
2019-09-10 23:57:13 +0800

    + decorate each field column of Student

--- src/server/entity/student.ts
+++ src/server/entity/student.ts
@@ -3,13 +3,21 @@ import { IStudent } from "../../types";
 
 @Entity()
 export class Student implements IStudent{
+    @Column()
     phone!: number;
+    @Column()
     age!: number
+    @Column()
     key?: number  ;
+    @Column()
     account!: string;
+    @Column()
     password!: string;
+    @Column()
     nickname!: string;
+    @Column()
     job!: string;
+    @Column()
     hobby!: string;
     @PrimaryGeneratedColumn()
     _id!: string;
