f289d2589cf5afab543b12ea14053a4ea3f7cec2
luqing110 <54201779+luqing110-ku@users.noreply.github.com>
2019-09-11 17:07:57 +0800

    加了git 命令 reg正则提取 英美

---
+++ git.md
@@ -0,0 +1,9 @@
+
+git pull
+
+git fetch upstream
+
+git checkout master
+
+
+git merge upstream/master
---
+++ src/commom/reg.ts
@@ -0,0 +1,8 @@
+	
+
+		 
+        export const en =  /(?<=英).*?(?=美)/g;
+
+         
+        export const mei = /(?<=美).*?\/ /g;
+   
