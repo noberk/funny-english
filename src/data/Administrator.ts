a68f3dc69443b77cdef668d377209f2293a205e5
noberk <noberk@users.noreply.github.com>
2019-09-20 23:23:24 +0800

    Export all words by alphabet

--- src/data/Administrator.ts
+++ src/data/Administrator.ts
@@ -3,10 +3,6 @@ export const Administrator : IAdministrator[]=
     {adminAccount : 'noberk', adminPassword: "123"},
     {adminAccount : 'luqing', adminPassword: "123"}
 ]
-
-
-
-
 interface IAdministrator{
       adminAccount:string;
       adminPassword:string
--- src/data/IELTS/index.ts
+++ src/data/IELTS/index.ts
@@ -1,17 +1,35 @@
-import {A} from "./A"
-import {B} from "./B"
-import {C} from "./C"
-import {D} from "./D"
-import {E} from "./E"
-import {F} from "./F"
-import {G} from "./G"
-import {H} from "./H"
-import {I} from "./I"
-import {JKL} from "./JKL"
-import {M} from "./M"
-import {P} from "./P"
-import {QR} from "./QR"
-import {S} from "./S"
-import {T} from "./T"
-import {UVWYZ} from "./UVWYZ"
+import * as a from "./A"
+import * as b from "./B"
+import * as c from "./C"
+import * as d from "./D"
+import * as e from "./E"
+import * as f from "./F"
+import * as g from "./G"
+import * as h from "./H"
+import * as i from "./I"
+import * as jkl from "./JKL"
+import * as m from "./M"
+import * as p from "./P"
+import * as qr from "./QR"
+import * as s from "./S"
+import * as t from "./T"
+import * as uvwyz from "./UVWYZ"
 
+const A = a.A;
+const B = b.B;
+const C = c.C;
+const D = d.D;
+const E = e.E;
+const F = f.F;
+const G = g.G;
+const I =i .I;
+const H = h.H;
+const M = m.M;
+const P = p.P;
+const S = s.S;
+const T = t.T;
+const JKL = jkl.JKL;
+const QR = qr.QR;
+const UVWYZ = uvwyz.UVWYZ
+
+export {A,B,C,D,E,F,G,I,H,M,P,S,T,JKL,QR,UVWYZ};
