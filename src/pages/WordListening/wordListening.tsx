66a7f0f0cd422f7a9a7946f741978988402bad65
noberk <noberk@users.noreply.github.com>
2019-09-30 18:51:02 +0800

    Fixed a bug where TS could not recognize type String to type enum

--- src/pages/WordListening/wordListening.tsx
+++ src/pages/WordListening/wordListening.tsx
@@ -1,75 +1,101 @@
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import { Button, Input } from "antd";
 import ButtonGroup from "antd/lib/button/button-group";
 import { guidGenerator } from "../../commom/id";
 import { H1, Center } from "../../components/styled";
- 
-import "./index.scss"
+
+import "./index.scss";
 import { essential4k_2_100 } from "../../data/essential_2";
 import { range } from "../../commom/arr";
 
-enum BookNumber{
-    ES4000,
-    IELTS,
-    TOEFL
+enum BookNumber {
+  ES4000,
+  IELTS,
+  TOEFL
 }
 
-const wordCountArray=[5,10,15,20,25,30,40,50,60,80,100]
-const wordList = essential4k_2_100.map(item=>{ return {word:item[0], definition: item[3]}});
+const wordCountArray = [5, 10, 15, 20, 25, 30, 40, 50, 60, 80, 100];
+const wordList = essential4k_2_100.map(item => {
+  return { word: item[0], definition: item[3] };
+});
 const bookList = [
-    { name: "📗ES4000" , type: BookNumber.ES4000},
-    { name: "📘IELTS" , type: BookNumber.IELTS},
-    { name: "📙TOEFL" , type: BookNumber.TOEFL},
-]
+  { name: "📗ES4000", type: BookNumber.ES4000 },
+  { name: "📘IELTS", type: BookNumber.IELTS },
+  { name: "📙TOEFL", type: BookNumber.TOEFL }
+];
 const WordListenning: React.FC = () => {
-    const [wCountArrary,setWCountArray]= useState(wordCountArray)
-    const [pickedWordCount,setPickedWordCount] = useState(5);
-    const [filteredWList] = useState(wordList.slice(0,10));
-    const [book,setBook] = useState(0);
-    const createPageCard= (number:number)=>{
-          return range(number);
+  const [wCountArrary, setWCountArray] = useState(wordCountArray);
+  const [pickedWordCount, setPickedWordCount] = useState(5);
+  const [filteredWList] = useState(wordList.slice(0, 10));
+  const [book, setBook] = useState(BookNumber.ES4000);
+
+  const createPageCard = (number: number) => {
+    return range(number);
+  };
+  const onWordSizeChanged = (e: any) => {
+    let pickedCount = Number.parseInt(e.target.name);
+    setPickedWordCount(pickedCount);
+  };
+  const onSelectBook = (e: any) => {
+    switch (e.target.name) {
+        case "0":setBook(BookNumber.ES4000);break;
+        case "1":setBook(BookNumber.IELTS);break;
+        case "2":setBook(BookNumber.TOEFL);break;    
     }
-    const onWordSizeChanged= (e: any)=>{
-       let pickedCount =  Number.parseInt(e.target.name);
-        setPickedWordCount(pickedCount);
-    }
+  };
+  
   return (
     <>
       <header id="wordlistenning-header">
         <H1>To select a word book to test your vocabulary</H1>
         <div>
-         <h1> To Choose your Book </h1>
+          <h1> To Choose your Book </h1>
           <ButtonGroup>
-           <Button>📗ES4000</Button>
-            <Button >📘IELTS</Button>
-            <Button>📙TOEFL</Button>
+            {bookList.map(bk => (
+              <Button
+                key={guidGenerator()}
+                type={bk.type === book ? "primary" : "default"}
+                name={bk.type.toString()}
+                onClick={onSelectBook}
+              >
+                {bk.name}
+              </Button>
+            ))}
           </ButtonGroup>
         </div>
 
         <div>
-            <h1>  To Set how many words you want to test in this paper.</h1>
+          <h1> To Set how many words you want to test in this paper.</h1>
           <ButtonGroup>
-            
-            {wCountArrary.map(count=> <Button onClick={onWordSizeChanged} key={guidGenerator()} name={count.toString()}>{count}</Button>)}
+            {wCountArrary.map(count => (
+              <Button
+                onClick={onWordSizeChanged}
+                key={guidGenerator()}
+                name={count.toString()}
+              >
+                {count}
+              </Button>
+            ))}
           </ButtonGroup>
         </div>
       </header>
       <br />
       <Center className="wordlistenning-article">
+        <div>🔊 (press key = to play sound again)</div>
         <div>
-          🔊 (press key = to play sound again)
-        </div>
-        <div>
-          <Input style={{width:400,height:100, fontSize:"3rem"}} />
+          <Input style={{ width: 400, height: 100, fontSize: "3rem" }} />
         </div>
         <div>this is a definition of this word.</div>
       </Center>
 
       <Center className="wordlistenning-pageCard">
-         {createPageCard(pickedWordCount).map(i=><Button key={guidGenerator()}>{i}</Button> )}
+        {createPageCard(pickedWordCount).map(i => (
+          <Button key={guidGenerator()}>{i}</Button>
+        ))}
       </Center>
     </>
   );
 };
 
 export default WordListenning;
+
