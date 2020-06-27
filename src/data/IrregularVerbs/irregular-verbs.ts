2ecf3fbff242db79558ec0607446674837a76a0e
noberk <noberkli@gmail.com>
2020-06-27 05:18:13 +0000

    Another more 20 irregular verbs have been added

--- src/data/IrregularVerbs/irregular-verbs.ts
+++ src/data/IrregularVerbs/irregular-verbs.ts
@@ -1,32 +1,52 @@
-interface VerbForms{
-    infinitive:string,
-    simplePast:string,
-    pastParticiple:string
-    presentParticiple:string
-
+interface VerbForms {
+  infinitive: string
+  simplePast: string
+  pastParticiple: string
+  presentParticiple: string
 }
 //{infinitive:'', simplePast:'',pastParticiple:''},
-export const irverb: Partial<VerbForms>[]=[
-    {infinitive:'arise', simplePast:'arose',pastParticiple:'arisen'},
-    {infinitive:'awake', simplePast:'awoke(awaked)',pastParticiple:'awoke (awaked, awoken)'},
-    {infinitive:'be', simplePast:'was / were',pastParticiple:'been'},
-    {infinitive:'become', simplePast:'became',pastParticiple:'become'},
-    {infinitive:'begin', simplePast:'began',pastParticiple:'begun'},
-    {infinitive:'bet', simplePast:'bade (bid)',pastParticiple:'bidden (bid)'},
-    {infinitive:'bid', simplePast:'arose',pastParticiple:'arisen'},
-    {infinitive:'bite', simplePast:'bit',pastParticiple:'bitten'},
-    {infinitive:'bleed', simplePast:'bled',pastParticiple:'bled'},
+export const irverb: Partial<VerbForms>[] = [
+  { infinitive: 'arise', simplePast: 'arose', pastParticiple: 'arisen' },
+  { infinitive: 'awake', simplePast: 'awoke(awaked)', pastParticiple: 'awoke (awaked, awoken)' },
+  { infinitive: 'be', simplePast: 'was / were', pastParticiple: 'been' },
+  { infinitive: 'become', simplePast: 'became', pastParticiple: 'become' },
+  { infinitive: 'bear', simplePast: 'bore', pastParticiple: 'born(e)' },
+  { infinitive: 'beat', simplePast: 'beat', pastParticiple: 'beaten' },
+  { infinitive: 'begin', simplePast: 'began', pastParticiple: 'begun' },
+  { infinitive: 'bet', simplePast: 'bade (bid)', pastParticiple: 'bidden (bid)' },
+  { infinitive: 'bid', simplePast: 'arose', pastParticiple: 'arisen' },
+  { infinitive: 'bite', simplePast: 'bit', pastParticiple: 'bitten' },
 
-    {infinitive:'break', simplePast:'broke',pastParticiple:'broken'},
-    {infinitive:'bring', simplePast:'brought',pastParticiple:'brought'},
-    {infinitive:'build', simplePast:'built',pastParticiple:'built'},
-    {infinitive:'burn', simplePast:'burnt (burned)',pastParticiple:'burnt (burned)'},
-    {infinitive:'buy', simplePast:'bought',pastParticiple:'bought'},
-    {infinitive:'can', simplePast:'could',pastParticiple:'could'},
-    {infinitive:'catch', simplePast:'cought',pastParticiple:'cought'},
-    {infinitive:'choose', simplePast:'chose',pastParticiple:'chosen'},
-    {infinitive:'come', simplePast:'came',pastParticiple:'come'},
-    {infinitive:'cost', simplePast:'cost',pastParticiple:'cost'},
- 
+  { infinitive: 'bleed', simplePast: 'bled', pastParticiple: 'bled' },
+  { infinitive: 'break', simplePast: 'broke', pastParticiple: 'broken' },
+  { infinitive: 'bring', simplePast: 'brought', pastParticiple: 'brought' },
+  { infinitive: 'build', simplePast: 'built', pastParticiple: 'built' },
+  { infinitive: 'burn', simplePast: 'burnt (burned)', pastParticiple: 'burnt (burned)' },
+  { infinitive: 'buy', simplePast: 'bought', pastParticiple: 'bought' },
+  { infinitive: 'can', simplePast: 'could', pastParticiple: 'could' },
+  { infinitive: 'catch', simplePast: 'cought', pastParticiple: 'cought' },
+  { infinitive: 'choose', simplePast: 'chose', pastParticiple: 'chosen' },
+  { infinitive: 'come', simplePast: 'came', pastParticiple: 'come' },
+
+  { infinitive: 'cost', simplePast: 'cost', pastParticiple: 'cost' },
+  { infinitive: 'cut', simplePast: 'cut', pastParticiple: 'cut' },
+  { infinitive: 'do', simplePast: 'did', pastParticiple: 'done' },
+  { infinitive: 'draw', simplePast: 'drew', pastParticiple: 'drawn' },
+  { infinitive: 'dream', simplePast: 'dreamt (dreamed)', pastParticiple: 'dreamt (dreamed)' },
+  { infinitive: 'drink', simplePast: 'drank', pastParticiple: 'drunk' },
+  { infinitive: 'drive', simplePast: 'drove', pastParticiple: 'driven' },
+  { infinitive: 'eat', simplePast: 'ate', pastParticiple: 'eaten' },
+  { infinitive: 'fall', simplePast: 'fell', pastParticiple: 'fallen' },
+  { infinitive: 'feed', simplePast: 'fed', pastParticiple: 'fed' },
+
+  { infinitive: 'feel', simplePast: 'felt', pastParticiple: 'felt' },
+  { infinitive: 'fight', simplePast: 'fought    ', pastParticiple: 'fought' },
+  { infinitive: 'find', simplePast: 'found', pastParticiple: 'found' },
+  { infinitive: 'flee', simplePast: 'fled', pastParticiple: 'fled' },
+  { infinitive: 'fly', simplePast: 'flew', pastParticiple: 'flown' },
+  { infinitive: 'forbit', simplePast: 'forbade', pastParticiple: 'forbidden' },
+  { infinitive: 'forget', simplePast: 'forgot', pastParticiple: 'forgotten' },
+  { infinitive: 'forgive', simplePast: 'forgave', pastParticiple: 'forgiven' },
+  { infinitive: 'freeze', simplePast: 'forze', pastParticiple: 'forzen' },
+  { infinitive: 'get', simplePast: 'got', pastParticiple: 'got (gotten)' },
 ]
-
--- src/data/IrregularVerbs/meterial.md
+++ src/data/IrregularVerbs/meterial.md
@@ -1,96 +1,71 @@
- 
-catch	caught	caught
-choose	chose	chosen
-come	came	come
-cost	cost	cost
-cut	cut	cut
-do	did	done
-draw	drew	drawn
-dream	dreamt (dreamed)	dreamt (dreamed)
-drink	drank	drunk
-drive	drove	driven
-eat	ate	eaten
-fall	fell	fallen
-feed	fed	fed
-feel	felt	felt
-fight	fought	fought
-find	found	found
-flee	fled	fled
-fly	flew	flown
-forbit	forbade	forbidden
-forget	forgot	forgotten
-forgive	forgave	forgiven
-freeze	froze	frozen
-get	got	got (gotten)
-give	gave	given
-go	went	gone
-grow	grew	grown
-have	had	had
-hear	heard	heard
-hide	hid	hidden
-hurt	hurt	hurt
-keep	kept	kept
-know	knew	known
-lay	laid	laid
-lead	led	led
-learn	learnt (learned)	learnt (learned)
-leave	left	left
-lend	lent	lent
-let	let	let
-lose	lost	lost
-make	made	made
-mean	meant	meant
-meet	met	met
-pay	paid	paid
-read	read	read
-ride	rode	ridden
-ring	rang	rung
-rise	rose	risen
-run	ran	run
-say	said	said
-see	saw	seen
-seek	sought	sought
-sell	sold	sold
-send	sent	sent
-shake	shook	shaken
-shave	shaved	shaven (shaved)
-shoot	shot	shot
-show	showed	shown
-shut	shut	shut
-sing	sang	sung
-sink	sank	sunk
-sit	sat	sat
-sleep	slept	slept
-smell	smelt (smelled)	smelt (smelled)
-speak	spoke	spoken
-spell	spelt (spelled)	spelt (spelled)
-spend	spent	spent
-spill	spilt	spilt
-stand	stood	stood
-steal	stole	stolen
-strike	stuck	struck (stricken)
-swear	swore	sworn
-swim	swam	swum
-swing	swung	swung
-take	took	taken
-teach	taught	taught
-tear	tore	torn
-tell	told	told
-think	thought	thought
-throw	threw	thrown
-understand	understood	understood
-wear	wore	worn
-win	won	won
-write	wrote	written
+give gave given
+go went gone
+grow grew grown
+have had had
+hear heard heard
+hide hid hidden
+hurt hurt hurt
+keep kept kept
+know knew known
+lay laid laid
+lead led led
+learn learnt (learned) learnt (learned)
+leave left left
+lend lent lent
+let let let
+lose lost lost
+make made made
+mean meant meant
+meet met met
+pay paid paid
+read read read
+ride rode ridden
+ring rang rung
+rise rose risen
+run ran run
+say said said
+see saw seen
+seek sought sought
+sell sold sold
+send sent sent
+shake shook shaken
+shave shaved shaven (shaved)
+shoot shot shot
+show showed shown
+shut shut shut
+sing sang sung
+sink sank sunk
+sit sat sat
+sleep slept slept
+smell smelt (smelled) smelt (smelled)
+speak spoke spoken
+spell spelt (spelled) spelt (spelled)
+spend spent spent
+spill spilt spilt
+stand stood stood
+steal stole stolen
+strike stuck struck (stricken)
+swear swore sworn
+swim swam swum
+swing swung swung
+take took taken
+teach taught taught
+tear tore torn
+tell told told
+think thought thought
+throw threw thrown
+understand understood understood
+wear wore worn
+win won won
+write wrote written
 
-
- {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
-    {infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
+{infinitive:'', simplePast:'',pastParticiple:''},
