f39c3a4ff6e5aba4f617104fd4faf34424a7f7b6
noberk <noberkli@gmail.com>
2020-06-27 04:56:58 +0000

    Another 20 irregular verbs have been added to this project

---
+++ src/data/IrregularVerbs/irregular-verbs.ts
@@ -0,0 +1,32 @@
+interface VerbForms{
+    infinitive:string,
+    simplePast:string,
+    pastParticiple:string
+    presentParticiple:string
+
+}
+//{infinitive:'', simplePast:'',pastParticiple:''},
+export const irverb: Partial<VerbForms>[]=[
+    {infinitive:'arise', simplePast:'arose',pastParticiple:'arisen'},
+    {infinitive:'awake', simplePast:'awoke(awaked)',pastParticiple:'awoke (awaked, awoken)'},
+    {infinitive:'be', simplePast:'was / were',pastParticiple:'been'},
+    {infinitive:'become', simplePast:'became',pastParticiple:'become'},
+    {infinitive:'begin', simplePast:'began',pastParticiple:'begun'},
+    {infinitive:'bet', simplePast:'bade (bid)',pastParticiple:'bidden (bid)'},
+    {infinitive:'bid', simplePast:'arose',pastParticiple:'arisen'},
+    {infinitive:'bite', simplePast:'bit',pastParticiple:'bitten'},
+    {infinitive:'bleed', simplePast:'bled',pastParticiple:'bled'},
+
+    {infinitive:'break', simplePast:'broke',pastParticiple:'broken'},
+    {infinitive:'bring', simplePast:'brought',pastParticiple:'brought'},
+    {infinitive:'build', simplePast:'built',pastParticiple:'built'},
+    {infinitive:'burn', simplePast:'burnt (burned)',pastParticiple:'burnt (burned)'},
+    {infinitive:'buy', simplePast:'bought',pastParticiple:'bought'},
+    {infinitive:'can', simplePast:'could',pastParticiple:'could'},
+    {infinitive:'catch', simplePast:'cought',pastParticiple:'cought'},
+    {infinitive:'choose', simplePast:'chose',pastParticiple:'chosen'},
+    {infinitive:'come', simplePast:'came',pastParticiple:'come'},
+    {infinitive:'cost', simplePast:'cost',pastParticiple:'cost'},
+ 
+]
+
