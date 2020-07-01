ae75dd24657c4523e44690571bc0bea5724fcf6d
noberk <noberk@users.noreply.github.com>
2020-07-01 12:17:33 +0800

    a new article to us how to doing work effectively

---
+++ src/data/Reading/culture/effective.ts
@@ -0,0 +1,115 @@
+Entrepreneurs are always on a quest to win the race against time.As if that weren’t stressful enough, we also have to enhance our willpower and strengthen our self - discipline.If not, then all of the distractions flying around will stand in the way of us getting things done.
+
+Since I’m an avid reader, I’ve learned over the years that the best way to improve my time management and productivity skills have been through books.Of course, it can be overwhelming when you’re searching online or browsing in a book store for literature that can assist you in living a more fruitful life.That’s why I’ve made the decision easier for you by sharing with the 15 best time management and productivity books of all time.
+
+1. “The 7 Habits of Highly Effective People: Powerful Lessons in Personal Change” by Stephen R.Covey
+First published in 1989, “The 7 Habits of Highly Effective People” is regularly considered the best book to read if you want to improve your productivity both professionally and personally.It focuses on developing new habits so that you can stay - on - track as opposed to eliminating bad habits.Most notably, defining your goals and priorities.
+
+Covey uses the analogy of placing rocks, pebbles, and sand into a jar to help you define what’s really important.If you start by putting the little things, such as the pebbles or sand, the rocks won’t fit.However, if you begin with the rock there’s enough room for you to put the tinier items around them in the jar.
+
+Key quote: “The key is not to prioritize what’s on your scheduling app but to schedule your priorities.”
+
+Related: 5 Ways to Be a More Effective People Person
+
+2. “How to Stop Procrastinating: A Simple Guide to Mastering Difficult Tasks and Breaking the Procrastination Habit” by S.J.Scott
+If you’re struggling with procrastination, this is arguably the only book you’ll need to own.How to Stop Procrastinating is a down - to - earth and uses a specific framework to assist you in becoming action - oriented.Scott also shares his own experiences on how he overcame procrastination and explores the main reasons why we drag out feet.
+
+    Personally, I enjoyed how straightforward this book.The advice provided isn’t too complex.It simply encourages you to does this, then do this, and after do that.
+
+Key quote: “If you’re someone who procrastinates, then this bad habit is limiting your success in a variety of ways.If you don’t address this issue, then you’ll reduce the likelihood that you’ll achieve your major goals.”
+
+3.“Eat That Frog!: 21 Great Ways to Stop Procrastinating and Get More Done in Less Time” by Brian Tracy
+If your procrastination still persists then Eat That Frog! should help you get over that hump.Inspired by a famous Mark Twain quote, “Eat a live frog the first thing in the morning, and nothing worse will happen to you the rest of the day, ” author Brian Tracy details why and how you should tackle your most challenging task in the morning.
+
+    Additionally, Tracy shares successful time management techniques like how to set goals, get organized, practice the “Law of Three, ” and applying the 80 / 20 Rule.
+
+Key quote: “You can get control of your time and your life only by changing the way you think, work, and deal with the never - ending river of responsibilities that flows over you each day.You can get control of your tasks and activities only to the degree that you stop doing some things and start spending more time on the few activities that can really make a difference in your life.”
+
+4. “The 4 - Hour Workweek: Escape 9 - 5, Live Anywhere, and Join the New Rich”
+by Timothy Ferriss
+
+Timothy Ferriss shares his step - by - step guide on how anyone can start earning a monthly five - figure income by working just The 4 - Hour Workweek.As a result, you’ll be able to live the life you actually want without having to be just another participant of the daily rat race.
+
+If this sounds too good to be true, Ferriss includes over 50 practical tips and real - life case studies on how you can live more and work less.
+
+Key quote: “Being able to quit things that don't work is integral to being a winner.”
+
+5. “Organize Tomorrow Today: 8 Ways to Retrain Your Mind to Optimize Performance at Work and in Life” by Dr.Jason Selk and Tom Bartow
+Written by Dr.Jason Selk, director of mental training for the St.Louis Cardinals, and business coach Tom Bartow's Organize Tomorrow Today, this book outlines the eight most effective ways to optimize your organization. As a result, you’ll focus on process-oriented goals that will guide you in maximizing your time and breaking bad habits.
+
+Key quote: “Greatness is predicated on consistently doing things others can’t or won’t do.Simply put, success is not about being brilliant.It is about being consistent.”
+
+6. “Getting Things Done: The Art of Stress - Free Productivity” by David Allen
+Since it’s release in 2001, “Getting Things Done” has not only become one of the most influential business books of all - time.It’s also considered the book for personal organization.Although Allen has tweaked the book over the years, the main concept remains intact.When you have a clear mind, you’re able to organize your thoughts, spark creativity, and boost your productivity.
+
+Key quote: “Getting things done requires two basic components: defining(1) what “done” means(outcome) and(2) what “doing” looks like(action).”
+
+Related: 5 Tips to Improve Focus and Get Things Done
+
+7. “Deep Work: Rules for Focused Success in a Distracted World” by Cal Newport
+What exactly is “deep work”?Author and professor Cal Newport defines it as, “Professional activities performed in a state of distraction - free concentration that push your cognitive capabilities to their limit.These efforts create new value, improve your skill, and are hard to replicate.”
+
+The problem is that in an increasingly distracting world getting into this flow is easier said than done.The good news is that Newport outlines specific disciplines that will transform your mind and habits so that you can concentrate and jump into your work.
+
+Key quote: “To produce at your peak level you need to work for extended periods with full concentration on a single task free from distraction.”
+
+8. “Make Time: How to Focus on What Matters Every Day” by Jake Zeratsky and John Knapp
+Although released just in September 2018, I’m already chalking this selection up as an instant classic.I thoroughly enjoyed the friendly and original approach that Zeratsky and Knapp laid out in “Make Time.” For example, only highlighting one priority per day so that all of your time and energy are present for that specific task.
+
+Key quote: “Believe in your Highlight: It is worth prioritizing over random disruption.”
+
+9. “168 Hours: You Have More Time Than You Think” by Laura Vanderkam
+Did you know that we all have the same 168 Hours in a week ? So, how come some people are able to squeeze every minute out of this time ? Bestselling author Laura Vanderkam draws from the real - life stories of successful and happy individuals, Vanderkam discovered that they find creative ways to make time for the important stuff.
+
+Key quote: “The majority of people who claim to be overworked work less than they think they do, and many of the ways people work are extraordinarily inefficient.Calling something ‘work’ does not make it important or necessary.”
+
+10. “The Checklist Manifesto: How To Get Things Right” by Atul Gawande
+Relying on his past experiences as a surgeon, bestselling author Atul Gawande explains that we’re prone to failure thanks to the amount of knowledge surrounding us.The solution ? The Checklist Manifesto.
+
+Through riveting stories, Gawande describes what exactly checklists are, what they aren’t, and how they can assist you in succeeding at getting things done.
+
+Key quote: “We are besieged by simple problems...Checklists can provide protection.”
+
+11. “Surge: Your Guide to Put Any Idea Into Action” by Matt Kane, Steve Garguilo and Sergiy Skoryk
+So many of us have ideas that we’re passionate about.Unfortunately, we sit on these ideas waiting for the “perfect” time or opportunity to act.The reality is that there is no such thing as the right moment.“Surge” will provide you with the strategies to finally bring your ideas to life.
+
+Key quote: “If you want something different, it’s time to do something different.It’s time to act.”
+
+12. “The Power of Habit: Why We Do What Do in Life and Business” by Charles Duhigg
+Award - winning business reporter Charles Duhigg uses fascinating stories and scientific discoveries to clarify how habits work, ways to change existing patterns, and what can be done to establish new habits.By focusing on good habits we’re better equipped to achieve more than we ever imagined.The Power of Habit is definitely a must - read.
+
+Key quote: “The Golden Rule of Habit Change: You can't extinguish a bad habit, you can only change it.”
+
+13. “Time Warrior: How to Defeat Procrastination, People - pleasing, Self - doubt, Over - commitment, Broken Promises and Chaos” by Steve Chandler
+Time Warrior is an astute and digestible book that instructs readers to become “non - linear.” When you accomplish this you’ll start to manage your priorities and stop letting people - pleasing and fear hold you back.As a result, you’ll become motivated to hone -in on the big picture.
+
+Key quote: “Action is the answer.”
+
+14. “Essentialism: The Disciplined Pursuit of Less” by Greg McKeown
+I’d say that the key to productivity is being able to distinguish between what’s essential and what’s not.Thankfully, Essentialism can guide you in determining how to prioritize your tasks and cutting - out the extraneous stuff so that you only focus on what’s most important.
+
+Key quote: “Essentialism is not about how to get more things done; it’s about how to get the right things done.It doesn’t mean just doing less for the sake of less either.It is about making the wisest possible investment of your time and energy in order to operate at our highest point of contribution by doing only what is essential.”
+
+Related: Discipline Is What Leads to Success
+
+15. “15 Secrets Successful People Know About Time Management - The Productivity Habits of 7 Billionaires, 13 Olympic Athletes, 29 Straight - A Students, and 239 Entrepreneurs” by Kevin Kruse
+Kevin Kruse, a best - selling author and entrepreneur, asked 200 different successful business owners, athletes, and straight - a students, “What is your number one secret to productivity ?”
+
+After analyzing the results, Kruse found that they all shared 15 Secrets Successful People Know About Time Management.These include:
+
+Focusing on minutes, not hours.
+Doing one thing at a time.
+Not using to -do -lists.
+Beating procrastination with time travel.
+Making it home for dinner.
+Using a notebook.
+Processing their email only once a day.
+Avoiding meetings at all costs.
+    Saying “no” to almost everything.
+Following the 80 / 20 Rule.
+Delegating most tasks.
+Creating theme days.
+Touching things only one time.
+Establishing and following a morning routine.
+Maintaining their energy by getting enough sleep, eating healthy, and working in sprints.
+Key quote: “Actually, highly successful people don’t think about time much at all.Instead, they think about values, priorities, and consistent habits.”
