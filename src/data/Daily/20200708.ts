749012bd92f91d5c1691e4f5e766faa0607633c1
noberk <noberk@users.noreply.github.com>
2020-07-08 14:30:53 +0800

    daily studying record

---
+++ src/data/Daily/20200708.ts
@@ -0,0 +1,14 @@
+import { Routines } from "./types";
+
+const data: Routines = {
+  words: [
+    [
+      "detainee",
+      " /ˌdiːteɪˈniː/ ",
+      "pron",
+      "a person who is kept in prison, usually because of his or her political opinions",
+      "So I can speak with this detainee alone",
+      "",
+    ],
+  ],
+};
---
+++ src/data/Daily/types.ts
@@ -0,0 +1,5 @@
+import { TWordList } from "../ES4K";
+
+export interface Routines {
+  words: TWordList;
+}
