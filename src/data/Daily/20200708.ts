4b91ede63f85a50ebb43c86fdc9c732b712fa4f5
noberk <noberk@users.noreply.github.com>
2020-07-08 14:47:51 +0800

    to record new words which have learnt today

--- src/data/Daily/20200708.ts
+++ src/data/Daily/20200708.ts
@@ -4,11 +4,90 @@ const data: Routines = {
   words: [
     [
       "detainee",
-      " /ˌdiːteɪˈniː/ ",
+      "/ˌdiːteɪˈniː/",
       "pron",
       "a person who is kept in prison, usually because of his or her political opinions",
-      "So I can speak with this detainee alone",
+      "So I can speak with this detainee alone.",
+      "",
+    ],
+    [
+      "incident",
+      "/ˈɪnsɪdənt/",
+      "pron",
+      "something that happens, especially sth unusual or unpleasant",
+      "His bad behaviour was just an isolated incident.",
+      "",
+    ],
+    [
+      "solitary",
+      "/ˈsɒlətri/",
+      "adj",
+      "done alone; without other people",
+      "He led a solitary life.",
+      "",
+    ],
+    [
+      "confinement",
+      "/kənˈfaɪnmənt/",
+      "pron",
+      "the state of being forced to stay in a closed space, prison, etc., the act of putting sb there",
+      "years of confinement as a political prisoner.",
+      "",
+    ],
+    [
+      "inmate",
+      "/ˈɪnmeɪt/",
+      "pron",
+      "one of the people living in an institution such as a prison or a mental hospital ",
+      "He learned this song as an inmate at a Texas prison.",
+      "",
+    ],
+    [
+      "psychology",
+      "/saɪˈkɒlədʒi/",
+      "pron",
+      "the scientific study of the mind and how it influences behaviour",
+      "He studied philosophy and psychology at Cambridge.",
+      "",
+    ],
+    [
+      "institute",
+      "/ˈɪnstɪtjuːt/",
+      "pron",
+      "an organization that has a particular purpose, especially one that is connected with education or a particular profession; the building used by this organization",
+      "a research institute",
+      "",
+    ],
+    [
+      "mechanism",
+      "/ˈmekənɪzəm/",
+      "pron",
+      "a method or a system for achieving sth ",
+      "mechanisms for dealing with complaints from the general public",
+      "",
+    ],
+    [
+      "dilemma",
+      "/daɪˈlemə/",
+      "pron",
+      "a situation which makes problems, often one in which you have to make a very difficult choice between things of equal importance",
+      "to face a dilemma",
       "",
     ],
   ],
 };
+
+// carbohydrate
+// hormone
+// amino
+// acid
+// insulin
+// cortisol
+// adrenaline
+// tyrosine
+// implication
+// decisive
+// ration
+// satiate
+// rodents
+// engrave
