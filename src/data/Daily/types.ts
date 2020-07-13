7b0ec506b11b69b85f7184925f055113e54b9832
noberk <noberk@users.noreply.github.com>
2020-07-14 05:06:17 +0800

    find 2 indexs in an number array which the sum of 2 values is equal 9

--- src/data/Daily/types.ts
+++ src/data/Daily/types.ts
@@ -3,3 +3,20 @@ import { TWordList } from "../ES4K";
 export interface Routines {
   words: TWordList;
 }
+
+let arr = [4, 5, 1, 8, 15];
+
+arr = arr.filter((n) => n <= 9);
+
+let supplement = arr.map((n) => 9 - n);
+
+let index: any[] = [];
+arr.forEach((n, ni) => {
+  supplement.forEach((s, si) => {
+    if (s === n) {
+      index = [ni, si];
+    }
+  });
+});
+
+console.log(arr[index[0]] + arr[index[1]]);
