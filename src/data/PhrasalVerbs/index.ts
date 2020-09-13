8b251faaa2d5e32463dd600891793df8d5c7435b
noberk <noberk@users.noreply.github.com>
2020-09-13 17:13:09 +0800

    fill notepad

---
+++ src/data/PhrasalVerbs/index.ts
@@ -0,0 +1,9 @@
+export const data = `
+at heart = in reality
+as far as be concerned
+as regards=concerning; with respect to.
+aside from = apart from
+at a loss = not knowing what to do
+at a time 
+
+`;
