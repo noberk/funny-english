1fcf37e00c0b56e8837b829af60eadb4cabfeea3
noberk <noberk@users.noreply.github.com>
2020-10-09 22:10:53 +0800

    remove unnecessarry files

--- src/data/PhrasalVerbs/index.ts
+++
@@ -1,9 +0,0 @@
-export const data = `
-at heart = in reality
-as far as be concerned
-as regards=concerning; with respect to.
-aside from = apart from
-at a loss = not knowing what to do
-at a time 
-
-`;
